0931e518f5f3bc96d758c3aa0c3f8b17dd249539
aman <amanu4519@gmail.com>
2024-04-09 09:53:47 +0500

    pages

--- client/src/components/pages/login/index.jsx
+++ client/src/components/pages/login/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
-import { setProfile } from "../store/profileSlice";
+import { setProfile } from "../../store/profileSlice";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({
